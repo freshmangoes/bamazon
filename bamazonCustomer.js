ce37c14c936e740d1b9bffdbcc9de770fdf1b658
Kyle C <kylecilia@gmail.com>
2019-12-30 21:36:55 -0800

    Now uses a promisified version of the node SQL package to make inquirer and queries mesh better

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,20 +1,32 @@
 const inquirer = require(`inquirer`);
-const mysql = require(`mysql`);
+const mysql2 = require(`mysql2/promise`);
 const helper = require(`./bcHelpers`);
 
-const con = mysql.createConnection({
-	host: `localhost`,
-	port: 8889,
-	user: `root`,
-	password: `root`,
-	database: `bamazon`
-});
+const bamazon = async () => {
+  const database =  await mysql2.createConnection({
+      host: `localhost`,
+      port: 8889,
+      user: `root`,
+      password: `root`,
+      database: `bamazon`
+  });
 
-const connection = con.connect((err, res) => {
-	if (err) throw err;
-	console.log(`res:: ${JSON.stringify(res, null, 2)}`);
-	console.log(`Connected :)`);
-  helper.initialTableQuery(con);
-  // helper.populatTable(con);
-  helper.getAllItems(con);
-});
+  try {
+    await helper.getAllItems(database); 
+    const productRes = await inquirer.prompt({
+			name: `product`,
+			type: `number`,
+			message: `What product ID would you like to buy?`
+		});
+
+		const quantityRes = await inquirer.prompt({
+			name: `quantity`,
+			type: `number`,
+			message: `Quantity you would like to buy?`
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+bamazon();
--- bcHelpers.js
+++ bcHelpers.js
@@ -1,3 +1,5 @@
+const inquirer = require(`inquirer`);
+
 const mockData = [
   [`iPhone 11 Pro`, `electronics`, 1100, 30],
   [`Ariat Groundbreaker`, `footwear`, 110, 10],
@@ -8,50 +10,73 @@ const mockData = [
   [`Bundle of Oranges`, `food`, 9, 1100]
 ];
 
-const initialTableQuery = (con) => {
-	const tableQuery = `CREATE TABLE IF NOT EXISTS products(
-    item_id INT NOT NULL auto_increment,
-    product_name VARCHAR(100) NOT NULL,
-    department_name VARCHAR(100) NOT NULL,
-    price INT NOT NULL,
-    stock_quantity INT NOT NULL,
-    PRIMARY KEY (item_id))`;
+// const initialTableQuery = (db) => {
+// 	const tableQuery = `CREATE TABLE IF NOT EXISTS products(
+//     item_id INT NOT NULL auto_increment,
+//     product_name VARCHAR(100) NOT NULL,
+//     department_name VARCHAR(100) NOT NULL,
+//     price INT NOT NULL,
+//     stock_quantity INT NOT NULL,
+//     PRIMARY KEY (item_id))`;
 
-	con.query(tableQuery, err => {
-		if (err) throw err;
-	});
-};
+// 	db.query(tableQuery, err => {
+// 		if (err) throw err;
+// 	});
+// };
 
 
-const populatTable = (con) => {
-  const tableQuery = `INSERT INTO PRODUCTS (product_name, department_name, price, stock_quantity) VALUES ?`;
-  con.query(tableQuery, [mockData], (err, res) => {
-    if(err) throw err;
-    console.log(`res:: ${JSON.stringify(res, null, 2)}`);
-  });
-};
+// const populatTable = (db) => {
+//   const tableQuery = `INSERT INTO PRODUCTS (product_name, department_name, price, stock_quantity) VALUES ?`;
+//   db.query(tableQuery, [mockData], (err, res) => {
+//     if(err) throw err;
+//     console.log(`res:: ${JSON.stringify(res, null, 2)}`);
+//   });
+// };
 
-const getAllItems = (con) => {
-  const query = `SELECT * FROM products`;
-  con.query(query, (err, res) => {
-    if(err) throw err;
-    // console.log(`res:: ${JSON.stringify(res, null, 2)}`);
-    console.log(`---------------------------------------------------`);
-    for(let i = 0; i < res.length; i++) {
-      console.log(`Item ID: ${res[i].item_id}`);
-      console.log(`Product Name: ${res[i].product_name}`);
-      // console.log(`Department: ${res[i].department_name}`);
-      console.log(`Price: $${res[i].price}`);
-      console.log(`Quantity Available: ${res[i].stock_quantity}`);
-      console.log(`---------------------------------------------------`);
+const getAllItems = async (db) => {
+  try {
+    // result from the database query
+    const res = await db.query(`SELECT * FROM products`);
+    // console.log(JSON.stringify(res[0], null, 2));
+    
+    // table data
+    const rows = res[0];
+    console.log(`---------------------------------------`);
+    // iterate through table data and print relevant data
+    for(let i = 0; i < rows.length; i++) {
+      console.log(`ID: ${rows[i].item_id}`);
+      console.log(`Product: ${rows[i].product_name}`);
+      console.log(`Price: $${rows[i].price}`);
+      console.log(`Quantity Available: ${rows[i].stock_quantity}`);
+      console.log(`---------------------------------------`);
     }
-  });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
+const customerInquiry = async (itemRes) => {
+	const productRes = await inquirer.prompt({
+		name: `product`,
+		type: `number`,
+		message: `What product ID would you like to buy?`
+	});
 
+	const quantityRes = await inquirer.prompt({
+		name: `quantity`,
+		type: `number`,
+		message: `Quantity you would like to buy?`
+  });
+
+  for(let i = 0; i < itemRes.length; i++) {
+    if(itemRes[i].product_id == productRes.product) {
+      console.log(itemRes[i]);
+    }
+  }
+};
 
 module.exports = {
-  initialTableQuery,
-  populatTable,
+  // initialTableQuery,
+  // populatTable,
   getAllItems,
 };
